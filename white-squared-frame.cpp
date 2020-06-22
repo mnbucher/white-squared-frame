8c7be2f11bb8f683a4920a30015359b2b040805e
Martin Juan José Bucher <bucherma@student.ethz.ch>
2020-06-22 12:24:09 +0200

    Remove flags as confusing resizing

--- white-squared-frame.cpp
+++ white-squared-frame.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 #include <string.h>
 #include <iostream>
-#include <Magick++.h> 
+#include <Magick++.h>
 
 using namespace std;
 using namespace Magick;
@@ -10,24 +10,20 @@ int main(int argc, char** argv) {
     InitializeMagick(*argv);
 
     try {
-        if(argc == 2){
-            Image image( "3200x3200", "white");
-            Image original("photo.jpg");
-            Image cropped = original;
+        Image image("3200x3200", "white");
+        Image original("photo.jpg");
+        Image cropped = original;
 
-            if(strcmp(argv[1], "w") == 0){                
-                cropped.resize("3000x2000");
-                image.composite(cropped, 100, 600, CopyCompositeOp);
-            }
-            else {
-                cropped.resize("2000x3000");
-                image.composite(cropped, 600, 100, CopyCompositeOp);
-            }
-            image.write( "photo-2.jpg"); 
-        }
+        printf("Resizing...\n");
+
+        cropped.resize("3000x2000");
+        image.composite(cropped, 100, 600, CopyCompositeOp);
+        image.write("photo-framed.jpg");
+
+        printf("Done!\n");
     }
     catch(Exception &error_) {
-        cout << "Caught exception: " << error_.what() << endl; 
+        cout << "Caught exception: " << error_.what() << endl;
         return 1;
     }
     return 0;
